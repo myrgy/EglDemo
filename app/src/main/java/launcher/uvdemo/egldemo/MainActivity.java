c7102b8b821b98b2f89a186db9579048d81f32f4
Alexander Dalshov <dalshov@gmail.com>
2016-06-18 00:23:11 +0300

    add opengl rendered

--- app/src/main/java/launcher/uvdemo/egldemo/MainActivity.java
+++ app/src/main/java/launcher/uvdemo/egldemo/MainActivity.java
@@ -1,13 +1,101 @@
 package launcher.uvdemo.egldemo;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        Log.d("MainActivity", "Has OpenGLES2: " + (hasGLES20()? "yes" : "no"));
+//    }
+//
+//    private boolean hasGLES20() {
+//        ActivityManager am = (ActivityManager)
+//                getSystemService(Context.ACTIVITY_SERVICE);
+//        ConfigurationInfo info = am.getDeviceConfigurationInfo();
+//        return info.reqGlEsVersion >= 0x20000;
+//    }
+//}
 
 public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView mSurfaceView;
+    private GLSurfaceView mGLView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        if (hasGLES20()) {
+            mGLView = new GLSurfaceView(this);
+            mGLView.setEGLContextClientVersion(2);
+            mGLView.setPreserveEGLContextOnPause(true);
+            mGLView.setRenderer(new GLES20Renderer());
+        } else {
+            // Time to get a new phone, OpenGL ES 2.0 not supported.
+        }
+
+        setContentView(mGLView);
+    }
+
+    private boolean hasGLES20() {
+        ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        ConfigurationInfo info = am.getDeviceConfigurationInfo();
+        return info.reqGlEsVersion >= 0x20000;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /*
+         * The activity must call the GL surface view's
+         * onResume() on activity onResume().
+         */
+        if (mSurfaceView != null) {
+            mSurfaceView.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        /*
+         * The activity must call the GL surface view's
+         * onPause() on activity onPause().
+         */
+        if (mSurfaceView != null) {
+            mSurfaceView.onPause();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
     }
 }
---
+++ app/src/main/java/launcher/uvdemo/egldemo/util/Util.java
@@ -0,0 +1,10 @@
+package launcher.uvdemo.egldemo.util;
+
+/**
+ * Created by mac on 18/06/16.
+ */
+
+public interface Util {
+    public static final boolean DEBUG = true;
+    public static final String LOG_TAG = "EglDemo";
+}

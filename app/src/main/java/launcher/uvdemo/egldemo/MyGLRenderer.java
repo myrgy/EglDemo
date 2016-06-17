3e9d92054fe2b61b31e63f68422d83c8fc01c0df
Alexander Dalshov <dalshov@gmail.com>
2016-06-18 01:58:23 +0300

    add opengl rendered

--- app/src/main/java/launcher/uvdemo/egldemo/MyGLRenderer.java
+++ app/src/main/java/launcher/uvdemo/egldemo/MyGLRenderer.java
@@ -25,9 +25,6 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         GLES20.glViewport(0, 0, width, height);
 
         float ratio = (float) width / height;
-
-        // this projection matrix is applied to object coordinates
-        // in the onDrawFrame() method
         Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
     }
 

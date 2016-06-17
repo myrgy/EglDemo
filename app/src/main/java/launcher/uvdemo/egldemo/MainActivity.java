fb550515c506acd18275024d4a1ede6381969ac2
Alexander Dalshov <dalshov@gmail.com>
2016-06-18 01:57:37 +0300

    add opengl rendered

--- app/src/main/java/launcher/uvdemo/egldemo/MainActivity.java
+++ app/src/main/java/launcher/uvdemo/egldemo/MainActivity.java
@@ -6,51 +6,25 @@ import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
-
-//public class MainActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        Log.d("MainActivity", "Has OpenGLES2: " + (hasGLES20()? "yes" : "no"));
-//    }
-//
-//    private boolean hasGLES20() {
-//        ActivityManager am = (ActivityManager)
-//                getSystemService(Context.ACTIVITY_SERVICE);
-//        ConfigurationInfo info = am.getDeviceConfigurationInfo();
-//        return info.reqGlEsVersion >= 0x20000;
-//    }
-//}
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     private GLSurfaceView mSurfaceView;
-    private GLSurfaceView mGLView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Log.d("MainActivity", "Has OpenGLES2: " + (hasGLES20()? "yes" : "no"));
+        setContentView(R.layout.activity_main);
 
-        if (hasGLES20()) {
-            mGLView = new GLSurfaceView(this);
-            mGLView.setEGLContextClientVersion(2);
-            mGLView.setPreserveEGLContextOnPause(true);
-            mGLView.setRenderer(new GLES20Renderer());
-        } else {
-            // Time to get a new phone, OpenGL ES 2.0 not supported.
+        mSurfaceView = (GLSurfaceView)findViewById(R.id.openglView);
+        if (mSurfaceView!= null) {
+            mSurfaceView.setEGLContextClientVersion(2);
+            mSurfaceView.setPreserveEGLContextOnPause(true);
+            mSurfaceView.setRenderer(new MyGLRenderer());
         }
-
-        setContentView(mGLView);
     }
 
     private boolean hasGLES20() {
@@ -62,10 +36,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        /*
-         * The activity must call the GL surface view's
-         * onResume() on activity onResume().
-         */
         if (mSurfaceView != null) {
             mSurfaceView.onResume();
         }
@@ -74,11 +44,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-
-        /*
-         * The activity must call the GL surface view's
-         * onPause() on activity onPause().
-         */
         if (mSurfaceView != null) {
             mSurfaceView.onPause();
         }
---
+++ app/src/main/java/launcher/uvdemo/egldemo/MyGLRenderer.java
@@ -0,0 +1,52 @@
+package launcher.uvdemo.egldemo;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+
+import javax.microedition.khronos.opengles.GL10;
+
+public class MyGLRenderer implements GLSurfaceView.Renderer {
+
+    private Triangle mTriangle;
+    private final float[] mMVPMatrix = new float[16];
+    private final float[] mProjectionMatrix = new float[16];
+    private final float[] mViewMatrix = new float[16];
+
+    @Override
+    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+        GLES20.glClearColor(0f, 0f, 0f, 1f);
+        mTriangle = new Triangle();
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 unused, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+
+        float ratio = (float) width / height;
+
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 unused) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
+        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+
+        mTriangle.draw(mMVPMatrix);
+    }
+
+    public static int loadShader(int type, String shaderCode) {
+        int shader = GLES20.glCreateShader(type);
+
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
+    }
+}
